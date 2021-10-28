89836cd0e85e0043f65f76d4fc23003cd15da894
John Chavez <jrc2855@gmail.com>
2021-10-27 23:11:38 -0400

    Lab 3 Modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
+import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container'
 import Header from './Header.js';
 import Footer from './Footer.js'
-import Main from './Main.js'
-import beastObj from './data.json'
+import Main from './Main.js';
+import SelectedBeast from './SelectedBeast';
 
-function App() {
-  return (
-    <Container>
-      <Header title = '301 Horned Beasts'/>
-      <Main message='These are the Beasts' beastObj={beastObj} />
-      <Footer text = 'Footer'/>
-    </Container>
-  );
+
+
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      selectedBeast:{},
+      showModal: false
+    }
+  }
+  changeBeast = (beast) => {
+    this.setState({ selectedBeast: beast })
+  }
+  showModal = () => {
+    this.setState({ showModal: true})
+  }
+  hideModal = () => {
+    this.setState({ showModal: false})
+  }
+
+  render() {
+
+    return (
+
+      <Container>
+        <Header title = "Horned Beasts"/>
+        <Main 
+        changeBeast={this.changeBeast}
+        showModal={this.showModal}/>
+        <Footer />
+        <SelectedBeast
+        beast={this.state.selectedBeast}
+        hideModal={this.hideModal}
+        showModal={this.state.showModal}/>
+      </Container>
+    )
+  }
 }
-
-
-export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,82 +1,22 @@
 import { Component } from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
+import beastObj from './data.json'
+import SelectedBeast from './SelectedBeast'
 
-class Main extends Component {
+export default class Main extends Component {
+
   render(){
-
-    const objs = this.props.beastObj;
-
     return(
+
       <Container>
         <Row>
-          <Col><HornedBeast obj={objs[0]} /></Col>
-          <Col><HornedBeast obj={objs[1]} /></Col>
-          <Col><HornedBeast obj={objs[2]} /></Col>
-        </Row>
-        <Row>
-          <Col><HornedBeast obj={objs[3]} /></Col>
-          <Col><HornedBeast obj={objs[4]} /></Col>
-          <Col><HornedBeast obj={objs[5]} /></Col>
-        </Row>
-        <Row>
-          <Col><HornedBeast obj={objs[6]} /></Col>
-          <Col><HornedBeast obj={objs[7]} /></Col>
-          <Col><HornedBeast obj={objs[8]} /></Col>
-        </Row>
-        <Row>
-          <Col><HornedBeast obj={objs[9]} /></Col>
-          <Col><HornedBeast obj={objs[10]} /></Col>
-          <Col><HornedBeast obj={objs[11]} /></Col>
-        </Row>
-        <Row>
-          <Col><HornedBeast obj={objs[12]} /></Col>
-          <Col><HornedBeast obj={objs[13]} /></Col>
-          <Col><HornedBeast obj={objs[14]} /></Col>
-        </Row>
-        <Row>
-          <Col><HornedBeast obj={objs[15]} /></Col>
-          <Col><HornedBeast obj={objs[16]} /></Col>
-          <Col><HornedBeast obj={objs[17]} /></Col>
+         {beastObj.map((beast) => 
+         (<SelectedBeast key={beast.id}
+         changeBeast={this.props.changeBeast}
+         showModal={this.props.showModal}/>))} 
         </Row>
       </Container>
     );
   };
 };
-
-class HornedBeast extends Component{
-
-  constructor(props){
-     super(props)
-
-    this.state = {
-      likes:0
-    }
-  }
-
-  handleClick = () => {
-    this.setState({ likes: this.state.likes + 1});
-  }
-  render(){
-    return(
-      <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" onClick={this.handleClick} src={this.props.obj.image_url} />
-        <Card.Body>
-          <Card.Title>{this.props.obj.title}</Card.Title>
-          <Card.Text>
-            {this.props.obj.description}
-            <h3>{this.state.likes} Likes </h3>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    )
-  }
-}
-
-// https://react-bootstrap.github.io/components/cards/
-// Where I learned more about the Cards.
-
-
-export default Main;
